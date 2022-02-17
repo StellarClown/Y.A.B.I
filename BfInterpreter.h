7a209ad97fa1e9384dc62cac1a4e80b493aef4b4
StellarClown <salvo.sl70@gmail.com>
2022-02-17 21:56:55 +0100

    Various bug fixes

--- BfInterpreter.h
+++ BfInterpreter.h
@@ -6,9 +6,14 @@ class BfInterpreter {
 	private:
 		string _instructions;
 		bool errorSyntax = false;
-		bool checkSyntax(const string& toCheck) const;
-		unsigned matchingBrackets(const string& cmd, const unsigned& pos) const;
+		bool checkSyntax(const string&) const;
+		unsigned matchingBrackets(const string&, const unsigned&) const;
 	public:
-		BfInterpreter(const string& commands);
-		void runCode();
+		BfInterpreter(const string&);
+		BfInterpreter(const char*); //Overloading constructor method for char*
+		BfInterpreter(const BfInterpreter&); //Overloading constructor method for copy from an existing object
+		~BfInterpreter() = default;
+		void operator= (const string&);
+		void operator= (const char*); //Overloading operator = for the char*
+		void runCode() const;
 };
